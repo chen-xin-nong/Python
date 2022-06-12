c07d74792d09b7989b23d37d7b8913e47840e5ce
chen_xin_nong <chen_xin_nong@163.com>
2022-06-12 17:32:21 +0800

    task_greeting.py

---
+++ task_greeting.py
@@ -0,0 +1,8 @@
+"""
+What is your surname: xxx
+Please input your given name: xxx
+Hello, xxx xxx 
+"""
+surname = input("What is your surname: ")
+name = input("Please input your given name: ")
+print("Hello " + surname + " " + name)
