5ebd96de2996ca765a7e668df973d8891a2b922b
chen_xin_nong <chen_xin_nong@163.com>
2022-06-14 18:14:43 +0800

    print the length of name

--- task_greeting.py
+++ task_greeting.py
@@ -2,7 +2,19 @@
 What is your surname: xxx
 Please input your given name: xxx
 Hello, xxx xxx 
+the length of your surname = 6
+the length of your given name = 7
 """
+
+
+
 surname = input("What is your surname: ")
 name = input("Please input your given name: ")
 print("Hello " + surname + " " + name)
+a = len(surname)
+b = str(a)
+print("the length of your surname = " + b)
+# print(len(surname))
+c = len(name)
+print("the length of your givenname = " + str(c))
+# print(len(name))
