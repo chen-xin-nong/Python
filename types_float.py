8f8288fada238a2edacd02ed884bda1dff55107f
chen_xin_nong <chen_xin_nong@163.com>
2022-06-20 17:36:04 +0800

    types_float

---
+++ types_float.py
@@ -0,0 +1,16 @@
+a = 3.0
+print(a)
+print(type(a))
+
+print("------------------------")
+b = 3
+print(b)
+print(type(b))
+
+
+print("------------------------")
+b = "3"
+print(b)
+print(type(b))
+
+
