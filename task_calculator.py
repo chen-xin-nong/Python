bac0a9fc818ce2691a0a66b5f8afbf27fd98bbaa
chen_xin_nong <chen_xin_nong@163.com>
2022-06-20 17:34:43 +0800

    input floating number

--- task_calculator.py
+++ task_calculator.py
@@ -8,9 +8,20 @@
 # int("6")
 
 
-a = input("请输入一个整数： ")
-b = input("请再输入一个整数： ")
-c = int(a)
-h =int(b)
-var = c + h
-print(a + " + " + b + " = " + str(var))
+# a = input("请输入一个整数： ")
+# b = input("请再输入一个整数： ")
+# c = int(a)
+# h = int(b)
+# var = c + h
+# print(a + " + " + b + " = " + str(var))
+
+
+# print(f"{a} + {b} = {var}")
+
+
+a = input("请输入一个小数： ")
+b = input("请再输入一个小数： ")
+c = float(a)
+d = float(b)
+e = c + d
+print(f"{a} + {b} = {e}")
