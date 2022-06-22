4ca728e9be05d58208000d66fab762b374760d98
chen_xin_nong <chen_xin_nong@163.com>
2022-06-22 17:18:26 +0800

    move codes into method

--- task_greeting.py
+++ task_greeting.py
@@ -7,14 +7,15 @@ the length of your given name = 7
 """
 
 
+def add():
+    surname = input("What is your surname: ")
+    name = input("Please input your given name: ")
+    print("Hello " + surname + " " + name)
+    a = len(surname)
+    b = str(a)
+    print("the length of your surname = " + b)
+    c = len(name)
+    print("the length of your givenname = " + str(c))
 
-surname = input("What is your surname: ")
-name = input("Please input your given name: ")
-print("Hello " + surname + " " + name)
-a = len(surname)
-b = str(a)
-print("the length of your surname = " + b)
-# print(len(surname))
-c = len(name)
-print("the length of your givenname = " + str(c))
-# print(len(name))
+
+add()
