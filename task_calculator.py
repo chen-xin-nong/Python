14c4e381d5fe54c440387e987ee65cce98a74635
chen_xin_nong <chen_xin_nong@163.com>
2022-06-22 17:17:55 +0800

    move codes into method

--- task_calculator.py
+++ task_calculator.py
@@ -7,21 +7,24 @@
 # str(6)
 # int("6")
 
-
-# a = input("请输入一个整数： ")
-# b = input("请再输入一个整数： ")
-# c = int(a)
-# h = int(b)
-# var = c + h
-# print(a + " + " + b + " = " + str(var))
-
-
-# print(f"{a} + {b} = {var}")
+def integer():
+    a = input("请输入一个整数： ")
+    b = input("请再输入一个整数： ")
+    c = int(a)
+    h = int(b)
+    var = c + h
+    print(a + " + " + b + " = " + str(var))
 
 
-a = input("请输入一个小数： ")
-b = input("请再输入一个小数： ")
-c = float(a)
-d = float(b)
-e = c + d
-print(f"{a} + {b} = {e}")
+
+
+def desim():
+    f = input("请输入一个小数： ")
+    g = input("请再输入一个小数： ")
+    c = float(f)
+    d = float(g)
+    e = c + d
+    print(f"{c} + {d} = {e}")
+
+integer()
+desim()
