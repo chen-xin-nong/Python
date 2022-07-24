231c9ddbedcd7ba720a487d26fd5a359c06c32a6
chen_xin_nong <chen_xin_nong@163.com>
2022-07-24 18:25:32 +0800

    method.py

--- method.py
+++ method.py
@@ -7,6 +7,37 @@ def add():
     print("3 + 5 = 8")
     print("how are you?")
 
-greet()
-add()
-add()
+
+def display(a):
+    b = "b"
+    c = "c"
+    print(b)
+    print(c)
+    print(a)
+
+
+
+# greet()
+# add()
+# add()
+# b = "fdfdg"
+# display(c)
+
+# c ="abc"
+# display(c)
+
+# a = "hello world"
+# display(a)
+
+# display("hello world")
+
+# display(a = "hello world")
+
+
+def sum(a: int, b: int):
+    print(a + b)
+    print(a - b)
+sum(22, 11)
+
+
+
